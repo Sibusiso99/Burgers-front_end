6c8ee514bf8b3bcc7a738f71a8d5529b3948337d
unknown <cburadebe@gmail.com>
2023-08-11 05:51:34 +0200

    update

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
+
+    const [burger,setBurger] = useState({
+        name:"",
+        description:"",
+        price:null,
+        cover:""
+    })
+
+    const handleChange = (e) =>{
+        setBurger(prev=>({...prev,[e.target.name]: e.target.value}));
+    };
+    console.log(burger)
     return (
-        <div>Add</div>
+        <div className='form'>
+            <h1>Add Burger</h1>
+            <input type="text" placeholder="name" onChange={handleChange } name="name"/>
+            <input type="text" placeholder="description" onChange={handleChange } name="description"/>
+            <input type="number" placeholder="price" onChange={handleChange } name="price"/>
+            <input type="text" placeholder="cover" onChange={handleChange } name="cover" />
+        </div>
     )
 }
 
--- src/pages/Burgers.jsx
+++ src/pages/Burgers.jsx
@@ -20,12 +20,18 @@ const Burgers = () => {
     return <div>
         <h1>Sibusiso Burgers</h1>
         <div className="burgers">
-            {burgers.map(burger=>(
-                <div>
+            {burgers.map((burger)=>(
+                <div className="burger" key={burger.id}>
                     {burger.cover&&<img src={burger.cover} alt="" />}
+                    <h2>{burger.name}</h2>
+                    <p>{burger.description}</p>
+                    <span>{burger.price}</span>
                 </div>
             ))}
         </div>
+        <button>
+           <a href="/add">Add Burger</a>
+        </button>
     </div>
 }
 
